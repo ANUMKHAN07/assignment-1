6603bdb1a67f8e4e7b2828bb0fc4cb45e7842be7
Anum Khan <anumkhan083@yahoo.com>
2019-11-05 19:47:07 +0500

    Assignmnent

---
+++ q4.py
@@ -0,0 +1,4 @@
+# compute area of a circle 
+r = float(input("Enter radius of a circle: "))
+area = 22/7*r*r
+print("area of circle having radius " + str(r) + " is: " + str(area))
---
+++ q6.py
@@ -0,0 +1,5 @@
+# Write a python program which takes two inputs from user and print them
+#addition
+first_input = input("Enter first input: ")
+second_input = input("Enter second input: ")
+print(first_input + second_input)
